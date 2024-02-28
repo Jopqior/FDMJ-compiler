95a6bda4ea8dbbc680411d3f84a7c9a0aa6143f5
Lin-Yanjun <20307130136@fudan.edu.cn>
2024-02-28 13:55:25 +0800

    update table.h/c

--- hw1/include/utils/dsa/table.h
+++ hw1/include/utils/dsa/table.h
@@ -1,3 +1,6 @@
+#ifndef TABLE_H
+#define TABLE_H
+
 /*
  * table.h - generic hash table
  *
@@ -7,13 +10,28 @@
  *  well-typed arguments and call the TAB_ functions.
  */
 
+#define TABSIZE 127
+
 typedef struct TAB_table_ *TAB_table;
+typedef struct binder_ *binder;
+
+struct binder_ {
+  void *key;
+  void *value;
+  binder next;
+  void *prevtop;
+};
+
+struct TAB_table_ {
+  binder table[TABSIZE];
+  void *top;
+};
 
 /* Make a new table mapping "keys" to "values". */
 TAB_table TAB_empty(void);
 
-/* Enter the mapping "key"->"value" into table "t", 
- *    shadowing but not destroying any previous binding for "key". */
+/* Enter the mapping "key"->"value" into table "t",
+ *  shadowing but not destroying any previous binding for "key". */
 void TAB_enter(TAB_table t, void *key, void *value);
 
 /* Look up the most recent binding for "key" in table "t" */
@@ -25,8 +43,10 @@ void *TAB_pop(TAB_table t);
 
 
 /* Call "show" on every "key"->"value" pair in the table,
- *  including shadowed bindings, in order from the most 
+ *  including shadowed bindings, in order from the most
  *  recent binding of any key to the oldest binding in the table */
 void TAB_dump(TAB_table t, void (*show)(void *key, void *value));
 
+binder TAB_getBinder(TAB_table t, void *key);
 
+#endif
