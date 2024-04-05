144f6ccc7dcc73efadacee2e07c173aeef028ec6
Lin-Yanjun <20307130136@fudan.edu.cn>
2024-04-05 21:28:19 +0800

    hw7: upload runnable raw files (with TODO of semant.h|c translate.h|c)

---
+++ hw7/lib/frontend/semant.c
@@ -0,0 +1,7 @@
+#include "semant.h"
+#include "stdio.h"
+
+T_funcDeclList transA_Prog(FILE *out, A_prog p, int arch_size) {
+  fprintf(out, "TODO: translate during semant! See Tiger Book for designing details:)\n");
+  return NULL;
+}
