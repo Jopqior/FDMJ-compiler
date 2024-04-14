3d9ca227369cb388087e68d79ad9750d1937aece
Lin-Yanjun <20307130136@fudan.edu.cn>
2024-04-14 16:18:34 +0800

    hw7: clean semant.c

--- hw7/lib/frontend/semant.c
+++ hw7/lib/frontend/semant.c
@@ -2,14 +2,6 @@
 
 int SEM_ARCH_SIZE; // to be set by arch_size in transA_Prog
 
-expty ExpTy(Tr_exp exp, Ty_ty value, Ty_ty location) {
-  expty et = checked_malloc(sizeof(*et));
-  et->exp = exp;
-  et->value = value;
-  et->location = location;
-  return et;
-}
-
 void transError(FILE *out, A_pos pos, string msg) {
   fprintf(out, "(line:%d col:%d) %s\n", pos->line, pos->pos, msg);
   fflush(out);
