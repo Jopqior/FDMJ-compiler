943d5ffec4578ceefee80645d365bfc8bd02b6ef
Fanaesar <20307130147@fudan.edu.cn>
2024-05-09 00:06:08 +0800

    hw11: update ssa.c and Makefile

--- hw11/lib/optimizer/ssa.c
+++ hw11/lib/optimizer/ssa.c
@@ -5,7 +5,7 @@
 #define __DEBUG
 #undef __DEBUG
 
-AS_instrList AS_instrList_to_SSA(AS_instrList bodyil, G_graph fg, G_nodeList bg) {
+AS_instrList AS_instrList_to_SSA(AS_instrList bodyil, G_nodeList lg, G_nodeList bg){
   /* here is your implementation of translating to ssa */
   return bodyil;
 }
