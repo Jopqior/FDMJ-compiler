3ede2d676249b96750bc44539672d52685517267
Fanaesar <20307130147@fudan.edu.cn>
2024-05-30 02:24:04 +0800

    hw9_10 && hw11  public tests

--- hw11/tools/main.c
+++ hw11/tools/main.c
@@ -232,6 +232,7 @@ int main(int argc, const char * argv[]) {
   fprintf(stdout, "declare void @putint(i64)\n");
   fprintf(stdout, "declare void @putfloat(double)\n");
   fprintf(stdout, "declare i64 @getint()\n");
+  fprintf(stdout, "declare i64 @getch(i64)\n");
   fprintf(stdout, "declare float @getfloat()\n");
   fprintf(stdout, "declare i64* @getarray(i64)\n");
   fprintf(stdout, "declare i64* @getfarray(i64)\n");
